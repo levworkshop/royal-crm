4dad8715e6dcb870e37a40bc8848f09577096897
shai <train@shailev.com>
2022-05-01 20:49:23 +0300

    products - router and database

--- controllers/customers.js
+++ controllers/customers.js
@@ -12,25 +12,13 @@ const pool = mysql.createPool({
 });
 
 module.exports = {
-    // list: [],
-
     addCustomer: function (name, phone, email, countryId) {
-        // const name = process.argv.slice(2);
-
         if (!name || name.length === 0) {
             throw ('ERROR: name is empty');
         }
 
-        // this.list.push({
-        //     name: name,
-        //     id: this.list.length,
-        // });
-
         pool.getConnection(function (connErr, connection) {
             if (connErr) throw connErr; // not connected!
-
-            // const sql = "INSERT INTO customers(name, phone, email, country_id)" +
-            //     " VALUES('" + name + "','" + phone + "','" + email + "','" + countryId + "');";
 
             const sql = "INSERT INTO customers(name, phone, email, country_id)" +
                 " VALUES(?,?,?,?);";
@@ -48,10 +36,6 @@ module.exports = {
     },
 
     customersList: function (req, res) {
-        // this.list.forEach(customer => {
-        //     console.log(`ok. name: ${customer.name} was created.`);
-        // })
-
         pool.getConnection(function (connErr, connection) {
             if (connErr) throw connErr; // not connected!
 
--- controllers/products.js
+++ controllers/products.js
@@ -1,22 +1,50 @@
-module.exports = {
-    products: [],
+const mysql = require('mysql2');
+const config = require('../config/dev');
 
-    addProduct: function() {
-        const name = process.argv.slice(2);
-    
+const pool = mysql.createPool({
+    host: config.DB_HOST,
+    user: config.DB_USER,
+    password: config.DB_PASSWORD,
+    database: config.DB_DATABASE,
+    waitForConnections: true,
+    connectionLimit: 5,
+    queueLimit: 0
+});
+
+module.exports = {
+    addProduct: function (name, desc, price) {
         if (!name || name.length === 0) {
             throw ('ERROR: name is empty');
         }
-    
-        orders.push({
-            name: name,
-            id: this.products.length,
-        });        
+
+        pool.getConnection(function (connErr, connection) {
+            if (connErr) throw connErr; // not connected!
+
+            const sql = "INSERT INTO products(name, description, price)" +
+                " VALUES(?,?,?);";
+
+            connection.query(
+                sql,
+                [name, desc, price],
+                function (sqlErr, result, fields) {
+                    if (sqlErr) throw sqlErr;
+
+                    console.log(result);
+                });
+        });
     },
 
-    productsList: function() {
-        this.products.forEach(product => {
-            console.log(`ok. name: ${product.name} was created.`);
-        })
+    productsList: function (req, res) {
+        pool.getConnection(function (connErr, connection) {
+            if (connErr) throw connErr; // not connected!
+
+            const sql = "SELECT * FROM products";
+
+            connection.query(sql, function (sqlErr, result, fields) {
+                if (sqlErr) throw sqlErr;
+
+                res.send(result);
+            });
+        });
     }
 }
