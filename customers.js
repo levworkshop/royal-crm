bfdc42fe348d3b0dd789f29b34369b20e8c9efb2
shai <train@shailev.com>
2022-05-01 18:29:01 +0300

    working with mysql2 package to update and get data from database

--- customers.js
+++ customers.js
@@ -1,22 +1,60 @@
-module.exports = {
-    list: [],
+const mysql = require('mysql2');
 
-    addCustomer: function () {
-        const name = process.argv.slice(2);
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'apps',
+    database: 'royal_crm',
+    waitForConnections: true,
+    connectionLimit: 5,
+    queueLimit: 0
+});
+
+module.exports = {
+    // list: [],
+
+    addCustomer: function (name, phone, email, countryId) {
+        // const name = process.argv.slice(2);
 
         if (!name || name.length === 0) {
             throw ('ERROR: name is empty');
         }
 
-        this.list.push({
-            name: name,
-            id: this.list.length,
+        // this.list.push({
+        //     name: name,
+        //     id: this.list.length,
+        // });
+
+        pool.getConnection(function (connErr, connection) {
+            if (connErr) throw connErr; // not connected!
+
+            const sql = "INSERT INTO customers(name, phone, email, country_id)" +
+                " VALUES('" + name + "','" + phone + "','" + email + "','" + countryId + "');";
+
+            connection.query(sql, function (sqlErr, result, fields) {
+                if (sqlErr) throw sqlErr;
+
+                console.log(fields);
+                console.log(result);
+            });
         });
     },
 
-    customersList: function () {
-        this.list.forEach(customer => {
-            console.log(`ok. name: ${customer.name} was created.`);
-        })
+    customersList: function (req, res) {
+        // this.list.forEach(customer => {
+        //     console.log(`ok. name: ${customer.name} was created.`);
+        // })
+
+        pool.getConnection(function (connErr, connection) {
+            if (connErr) throw connErr; // not connected!
+
+            const sql = "SELECT * FROM customers";
+
+            connection.query(sql, function (sqlErr, result, fields) {
+                if (sqlErr) throw sqlErr;
+
+                res.send(result);
+            });
+        });
     }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const customersModule = require('../customers');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('hello again');
+  // res.send('hello again');
+  customersModule.addCustomer('Lola', '05233333', 'hi@gmail.com', 1);
+  customersModule.customersList(req, res);
 });
 
 module.exports = router;
