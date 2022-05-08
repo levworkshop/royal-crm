ec8400ba80d25606b7a6cdf97f57fe114300b024
shai <train@shailev.com>
2022-05-08 19:57:24 +0300

    new database controller

--- controllers/customers.js
+++ controllers/customers.js
@@ -1,15 +1,4 @@
-const mysql = require('mysql2');
-const config = require('../config/dev');
-
-const pool = mysql.createPool({
-    host: config.DB_HOST,
-    user: config.DB_USER,
-    password: config.DB_PASSWORD,
-    database: config.DB_DATABASE,
-    waitForConnections: true,
-    connectionLimit: 5,
-    queueLimit: 0
-});
+const database = require('./database');
 
 module.exports = {
     addCustomer: function (name, phone, email, countryId) {
@@ -17,7 +6,7 @@ module.exports = {
             throw ('ERROR: name is empty');
         }
 
-        pool.getConnection(function (connErr, connection) {
+        database.pool.getConnection(function (connErr, connection) {
             if (connErr) throw connErr; // not connected!
 
             const sql = "INSERT INTO customers(name, phone, email, country_id)" +
@@ -29,14 +18,14 @@ module.exports = {
                 function (sqlErr, result, fields) {
                     if (sqlErr) throw sqlErr;
 
-                    console.log(fields);
+                    // console.log(fields);
                     console.log(result);
                 });
         });
     },
 
     customersList: function (req, res) {
-        pool.getConnection(function (connErr, connection) {
+        database.pool.getConnection(function (connErr, connection) {
             if (connErr) throw connErr; // not connected!
 
             const sql = "SELECT * FROM customers";
--- controllers/orders.js
+++ controllers/orders.js
@@ -1,3 +1,5 @@
+const database = require('./database');
+
 module.exports = {
     orders: [],
 
