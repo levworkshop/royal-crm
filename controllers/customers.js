7edf9a26b9c2e725214a53964b87ffd630d9853c
shai <train@shailev.com>
2022-05-15 19:31:30 +0300

    data validation

--- controllers/customers.js
+++ controllers/customers.js
@@ -1,25 +1,37 @@
 const database = require('./database');
+const joi = require('joi');
 
 module.exports = {
     addCustomer: async function (req, res, next) {
-        const qs = req.body;
-        const name = qs.name;
-        const phone = qs.phone;
-        const email = qs.email;
-        const country = qs.countryInputHtml;
+        const reqBody = req.body;
 
-        if (!name || name.length === 0) {
-            throw ('ERROR: name is empty');
+        const schema = joi.object({
+            name: joi.string().required().min(2).max(200),
+            phone: joi.string().required().regex(/^[0-9]\d{8,11}$/),
+            email: joi.string().required(),
+            countryInputHtml: joi.number().required(),
+        });
+
+        const { error, value } = schema.validate(reqBody);
+
+        if (error) {
+            res.send(`error adding customer: ${error}`);
+            return;
         }
 
-        const sql = 
-        "INSERT INTO customers(name, phone, email, country_id)" +
+        const sql =
+            "INSERT INTO customers(name, phone, email, country_id)" +
             " VALUES(?,?,?,?);";
 
         try {
             const result = await database.query(
-                sql, 
-                [name, phone, email, country]
+                sql,
+                [
+                    reqBody.name,
+                    reqBody.phone,
+                    reqBody.email,
+                    reqBody.countryInputHtml
+                ]
             );
         }
         catch (err) {
@@ -27,7 +39,7 @@ module.exports = {
             return;
         }
 
-        res.send(`${name} added successfully`);
+        res.send(`${reqBody.name} added successfully`);
     },
 
     customersList: async function (req, res, next) {
@@ -47,13 +59,13 @@ module.exports = {
 
     // todo: delete customer
     // sql: DROP
-    deleteCustomer: async function(req, res, next) {
+    deleteCustomer: async function (req, res, next) {
 
     },
 
     // todo: export all customers to file
     // sql: SELECT
-    exportCustomers: async function(req, res, next) {
+    exportCustomers: async function (req, res, next) {
         const sql = "SELECT cust.name, cust.phone, cust.email, " +
             "cntr.name AS country_name FROM customers cust " +
             "LEFT JOIN countries cntr ON cust.country_id = cntr.id ORDER BY cust.name ASC;";
@@ -64,11 +76,11 @@ module.exports = {
 
     // todo: search in customers by parameter (name,email,country)
     // sql: SELECT WHERE
-    findCustomer: async function(req, res, next) {},
+    findCustomer: async function (req, res, next) { },
 
     // todo: edit/update customer
-    updateCustomer: async function(req, res, next) {},
+    updateCustomer: async function (req, res, next) { },
 
     // todo: view more details of a customer
-    viewCustomerDetails: async function(req, res, next) {},
+    viewCustomerDetails: async function (req, res, next) { },
 }
