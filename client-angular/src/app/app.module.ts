a67fdf06abf7059fa2f0931a62d80f971aa79480
levworkshop <noreply@githubusers.com>
2022-06-29 18:39:02 +0300

    refactor with module

--- client-angular/src/app/app.module.ts
+++ client-angular/src/app/app.module.ts
@@ -1,41 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { ButtonComponent } from './button/button.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
-import { CustomersComponent } from './customers/customers.component';
-import { ProductsComponent } from './products/products.component';
-import { OrdersComponent } from './orders/orders.component';
-import { SignupComponent } from './signup/signup.component';
-import { LoginComponent } from './login/login.component';
-import { SessionService } from './services/session.service';
+import { AuthModule } from './auth/auth.module';
+import { CoreModule } from './core/core.module';
+import { CustomersModule } from './customers/customers.module';
+import { HomeModule } from './home/home.module';
+import { OrdersModule } from './orders/orders.module';
+import { ProductsModule } from './products/products.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
     declarations: [
         AppComponent,
-        ButtonComponent,
-        HeaderComponent,
-        FooterComponent,
-        HomeComponent,
-        CustomersComponent,
-        ProductsComponent,
-        OrdersComponent,
-        SignupComponent,
-        LoginComponent
     ],
     imports: [
         BrowserModule,
-        ReactiveFormsModule,
-        AppRoutingModule
+        SharedModule,
+        AppRoutingModule,
+        AuthModule,
+        CoreModule,
+        CustomersModule,
+        HomeModule,
+        OrdersModule,
+        ProductsModule,
     ],
-    providers: [
-        SessionService
-    ],
+    providers: [],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
