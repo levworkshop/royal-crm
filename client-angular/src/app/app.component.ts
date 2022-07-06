24857486c9633305aaabd2c1ca33785f70a83e9c
levworkshop <noreply@githubusers.com>
2022-07-06 18:56:24 +0300

    theme dropdown

--- client-angular/src/app/app.component.ts
+++ client-angular/src/app/app.component.ts
@@ -8,6 +8,16 @@ import { SessionService } from './core/session.service';
 })
 export class AppComponent implements AfterViewInit {
 
+    themes = [{
+        title: 'Light',
+        value: 'light-theme'
+    }, {
+        title: 'Dark',
+        value: 'dark-theme'
+    }];
+
+    selectedTheme = 'light-theme';
+
     constructor(private sessionService: SessionService) { }
 
     ngAfterViewInit(): void {
--- client-angular/src/app/app.module.ts
+++ client-angular/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
@@ -17,6 +18,7 @@ import { SharedModule } from './shared/shared.module';
     ],
     imports: [
         BrowserModule,
+        FormsModule,
         SharedModule,
         AppRoutingModule,
         AuthModule,
