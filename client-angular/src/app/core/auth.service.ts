4009b0ad9cb6ab7424023049fb92f8d00ae5c2f6
levworkshop <noreply@githubusers.com>
2022-07-20 20:45:15 +0300

    guard

--- client-angular/src/app/core/auth.service.ts
+++ client-angular/src/app/core/auth.service.ts
@@ -3,14 +3,38 @@ import { Observable } from 'rxjs';
 import { Login, User } from '../shared/types';
 import { ApiService } from './api.service';
 import { tap } from 'rxjs/operators';
+import { ActivatedRouteSnapshot, CanActivateChild, Router, RouterStateSnapshot } from '@angular/router';
+
 
 @Injectable({
     providedIn: 'root'
 })
-export class AuthService {
+export class AuthService implements CanActivateChild {
     private readonly tokenField = 'token';
+    redirectUrl: string | null = null; // store url for redirecting after login
 
-    constructor(private apiService: ApiService) { }
+    constructor(
+        private router: Router,
+        private apiService: ApiService,
+    ) { }
+
+    canActivateChild(
+        childRoute: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot): boolean | Promise<boolean> {
+
+        // is the user logged in
+        if (this.isLoggedIn()) { return true };
+
+        // store the attempted url for redirecting
+        this.redirectUrl = state.url;
+
+        return this.router.navigate(['login-component']);
+    }
+
+    isLoggedIn(): boolean {
+        const token = localStorage.getItem(this.tokenField);
+        return token && token.length > 0 ? true : false;
+    }
 
     login(details: Login): Observable<User> {
         return this.apiService.login(details).pipe(
